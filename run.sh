3b69e3b0763f47238b533ea3051d038bdb6e0c44
yonxean <1617003785@qq.com>
2022-06-18 11:11:42 +0800

    the homework of AI based CNN

---
+++ run.sh
@@ -0,0 +1,12 @@
+#!/bin/bash
+dataset="voc"
+iters=200
+if [ $dataset = "voc" ]
+then
+    data_dir="./data/VOCdevkit/VOC2012/"
+elif [ $dataset = "coco" ]
+then
+    data_dir="./data/coco2017/"
+fi
+python train.py --use-cuda --iters ${iters} --dataset ${dataset} --data-dir ${data_dir}
+
